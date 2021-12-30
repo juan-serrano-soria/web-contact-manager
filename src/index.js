4af025d75e7c380ac8d9dd89506f794c1c0f356c
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2021-12-30 13:52:33 +0100

    First Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.css';
+
+ReactDOM.render(<h1>Web Contact Manager</h1>, document.getElementById('root'));
