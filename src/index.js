40451f9245b8ac515cfe8a3abc53280dc4a9ff15
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2021-12-30 14:08:05 +0100

    Simple increment decrement numClicks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-import React, { StrictMode } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import './style.css';
 
-ReactDOM.render(<h1>Web Contact Manager</h1>, document.getElementById('root'));
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const [numClicks, setNumClicks] = useState(0);
+
+  useEffect(() => {
+    alert('Number of clicks: ' + numClicks);
+  });
+
+  function increment() {
+    setCounter(counter + 1);
+    setNumClicks(numClicks + 1);
+  }
+
+  function decrement() {
+    setCounter(counter - 1);
+    setNumClicks(numClicks + 1);
+  }
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+const el = <Counter />;
+
+ReactDOM.render(el, document.getElementById('root'));
